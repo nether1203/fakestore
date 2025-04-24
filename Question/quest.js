c8cacf2cee6c3c877ae33b440eb269c259b25c51
xalys_ura <xalysjyra1992@gmail.com>
2025-04-24 21:22:42 +0300

    fix box question

--- Question/quest.js
+++ Question/quest.js
@@ -2,4 +2,6 @@
 function toggleText() {
     const text = document.getElementById('hiddenText');
     text.style.display = (text.style.display === 'block') ? 'none' : 'block';
+    const title = document.querySelector('.questionBoxTitle');
+  title.classList.toggle('questionTitleBig');
   }
