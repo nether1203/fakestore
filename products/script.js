4dffb8cde92b44d06e463186f3a652a81187d981
Marko <tarasenko.marko2007@gmail.com>
2025-04-24 17:15:02 +0200

    добавив 3 категорії

--- products/script.js
+++ products/script.js
@@ -1,5 +1,9 @@
 
 const productBox = document.querySelector('.productBox');
+const jewelery = document.querySelector('.jewelery');
+const mensClothing = document.querySelector('.mensClothing');
+const electronics = document.querySelector('.electronics');
+const womensClothing = document.querySelector('.womensClothing');
 
 fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
@@ -11,7 +15,7 @@ fetch('https://fakestoreapi.com/products')
   function products(data){
     data.forEach(item => {
         if (item.category == "men's clothing") {
-            productBox.innerHTML += `
+            mensClothing.innerHTML += `
             <a href="/product-detail/index.html?id=${item.id}" class="productContent">
                 <div >
                     <div class="productContentTop">
@@ -26,6 +30,40 @@ fetch('https://fakestoreapi.com/products')
             </a>
             
             `
-        } 
+        } /*else if (item.category == "jewelery") {
+            jewelery.innerHTML += `
+            <a href="/product-detail/index.html?id=${item.id}" class="productContent">
+                <div >
+                    <div class="productContentTop">
+                        <img src="${item.image}" alt="" class="productImg">
+                    </div>
+                    <img src="../src/img/Button.png" alt="" class="basket">
+                    <div class="productContentBotton">
+                        <h3>${item.title}</h3>
+                        <p>$${item.price}</p>
+                    </div>
+                </div>
+            </a>
+            
+            `
+        } else if (item.category == "women's clothing") {
+            womensClothing.innerHTML += `
+            <a href="/product-detail/index.html?id=${item.id}" class="productContent">
+                <div >
+                    <div class="productContentTop">
+                        <img src="${item.image}" alt="" class="productImg">
+                    </div>
+                    <img src="../src/img/Button.png" alt="" class="basket">
+                    <div class="productContentBotton">
+                        <h3>${item.title}</h3>
+                        <p>$${item.price}</p>
+                    </div>
+                </div>
+            </a>
+            
+            ` 
+        }*/
+            
+        
     });
   }
