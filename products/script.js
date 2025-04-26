041c06c4ff8e001c6176950143ba2bdfe71f0764
Marko <tarasenko.marko2007@gmail.com>
2025-04-26 15:00:40 +0200

    зробив адаптив

--- products/script.js
+++ products/script.js
@@ -1,5 +1,10 @@
 
-const cliderTrack = document.querySelector('.swiper-wrapper');
+const width = screen.width
+console.log(width);
+
+const cliderTrack = document.querySelectorAll('.cliderTrack');
+
+const swiperWrapper = document.querySelectorAll('.swiper-wrapper');
 
 fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
@@ -10,9 +15,10 @@ fetch('https://fakestoreapi.com/products')
 
   function products(data){
     data.forEach(item => {
-        if (item.category == "women's clothing") {
-            cliderTrack.innerHTML += `
-            <div href="http://127.0.0.1:5500/product-detail/index.html?id=${item.id}" class="sliderItem swiper-slide">
+        if (item.category == "men's clothing") {
+          if (width <= 1440) {
+            swiperWrapper[0].innerHTML += `
+            <a href="/product-detail/index.html?id=${item.id}" class="swiper-slide">
                 <div >
                     <div class="productContentTop">
                         <img src="${item.image}" alt="" class="productImg">
@@ -23,34 +29,94 @@ fetch('https://fakestoreapi.com/products')
                         <p>$${item.price}</p>
                     </div>
                 </div>
-            </div>
-            
+            </a>
             `
-        } 
+          } else {
+            cliderTrack[0].innerHTML += `
+            <a href="/product-detail/index.html?id=${item.id}" class="sliderItem">
+                <div >
+                    <div class="productContentTop">
+                        <img src="${item.image}" alt="" class="productImg">
+                    </div>
+                    <img src="../src/img/Button.png" alt="" class="basket">
+                    <div class="productContentBotton">
+                        <h3>${item.title}</h3>
+                        <p>$${item.price}</p>
+                    </div>
+                </div>
+            </a>
+            `
+          }
+        } else if(item.category == "jewelery") {
+          if (width <= 1440) {
+            swiperWrapper[1].innerHTML += `
+            <a href="/product-detail/index.html?id=${item.id}" class="swiper-slide">
+                <div >
+                    <div class="productContentTop">
+                        <img src="${item.image}" alt="" class="productImg jeweleryImg">
+                    </div>
+                    <img src="../src/img/Button.png" alt="" class="basket">
+                    <div class="productContentBotton">
+                        <h3>${item.title}</h3>
+                        <p>$${item.price}</p>
+                    </div>
+                </div>
+            </a>
+            `
+          } else {
+            cliderTrack[1].innerHTML += `
+            <a href="/product-detail/index.html?id=${item.id}" class="sliderItem">
+                <div>
+                    <div class="productContentTop">
+                        <img src="${item.image}" alt="" class="productImg jeweleryImg">
+                    </div>
+                    <img src="../src/img/Button.png" alt="" class="basket">
+                    <div class="productContentBotton">
+                        <h3>${item.title}</h3>
+                        <p>$${item.price}</p>
+                    </div>
+                </div>
+            </a>
+            `
+          }
+        } else if (item.category == "women's clothing") {
+          swiperWrapper[2].innerHTML += `
+            <a href="/product-detail/index.html?id=${item.id}" class="swiper-slide">
+                <div >
+                    <div class="productContentTop">
+                        <img src="${item.image}" alt="" class="productImg jeweleryImg">
+                    </div>
+                    <img src="../src/img/Button.png" alt="" class="basket">
+                    <div class="productContentBotton">
+                        <h3>${item.title}</h3>
+                        <p>$${item.price}</p>
+                    </div>
+                </div>
+            </a>
+            `
+        }
     });
 
-    
 
     
-
-
-    
-
-
   }
   let swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
+      clickable: false,
     },
     breakpoints: {
       640: {
         slidesPerView: 2,
         spaceBetween: 20,
       },
-      1024: {
+      1050: {
         slidesPerView: 3,
         spaceBetween: 40,
       },
@@ -60,4 +126,6 @@ fetch('https://fakestoreapi.com/products')
       }
     },
   });
+
+
   
