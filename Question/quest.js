f63cb6ec10a64e042f0eb81a35a994ffa43ef40a
Maksum <maksimkopis@gmail.com>
2025-04-26 17:48:43 +0300

    Last Version

--- Question/quest.js
+++ Question/quest.js
@@ -1,7 +1,18 @@
 
-function toggleText() {
-    const text = document.getElementById('hiddenText');
+// function toggleText() {
+//     const text = document.getElementById('hiddenText');
+//     text.style.display = (text.style.display === 'block') ? 'none' : 'block';
+//     const title = document.querySelector('.questionBoxTitle');
+//     title.classList.toggle('questionTitleBig');
+//   }
+
+const toggles = document.querySelectorAll('.svg');
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    const text = toggle.parentNode.parentNode.querySelector('#hiddenText');
+    console.log(toggle.parentNode.parentNode);
     text.style.display = (text.style.display === 'block') ? 'none' : 'block';
-    const title = document.querySelector('.questionBoxTitle');
-  title.classList.toggle('questionTitleBig');
-  }
+    const title = toggle.parentNode.querySelector('.questionBoxTitle');
+  });
+});
