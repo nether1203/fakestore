6abdd11253dad1aef23a2db3dbbb02074de96ea6
MaksymZaluskiy <zmax1301@gmail.com>
2025-04-27 21:54:53 +0300

    showing_and_unshowing_modal

--- product-detail/modal.js
+++ product-detail/modal.js
@@ -1,7 +1,11 @@
 const cart = document.querySelector(".shoppingCart")
 const modal = document.querySelector(".product_modal")
+const cross = document.querySelector(".cross")
 cart.addEventListener("click", ()=>{
     modal.classList.toggle("active")
+})
+cross.addEventListener("click", ()=>{
+    modal.classList.remove("active")
 })
 
 function randomId() {
