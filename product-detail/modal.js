642b130d36ec8d04383bbc317dc1729aef9b7a30
MaksymZaluskiy <zmax1301@gmail.com>
2025-04-30 20:30:16 +0300

    showing_products_and_calculating_sum

--- product-detail/modal.js
+++ product-detail/modal.js
@@ -3,6 +3,9 @@ const modal = document.querySelector(".product_modal")
 const cross = document.querySelector(".cross")
 const userName = document.querySelector(".user_name")
 const date = document.querySelector(".date")
+let inputs = document.querySelectorAll(".checkbox_example")
+const container = document.querySelector(".modal_container")
+const BlockSum = document.querySelector(".sum")
 
 cart.addEventListener("click", ()=>{
     modal.classList.toggle("active")
@@ -51,9 +54,54 @@ fetch(`https://fakestoreapi.com/carts/${Id}`)
 
         fetch(`https://fakestoreapi.com/products/${product.productId}`)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => {
 
-
-        
-    }
+                console.log(data)
+                    
+                container.innerHTML += `
+                            <div class="modal_item_example">
+                                        <div class="item_left">
+                                        <input class="checkbox_example" type="checkbox" name="add" data-count="${data.rating.count}" data-price="${data.price}" value="false">
+                                        <label class="added_product_name_example">${data.title}</label>
+                                        </div>
+                                        <div class="item_right">
+                                        <div class="added_product_count_example">Кількість: <span class="product_count">${data.rating.count}</span></div>
+                                        <div class="added_product_price_example">Ціна за 1: <span class="product_price">${data.price}</span>$</div>
+                                        </div>
+                                    </div>
+                        `
+                        inputs = document.querySelectorAll(".checkbox_example")
+                        console.log(inputs)
+            });
+    }             
 });
+
+let sum = 0
+container.addEventListener("click", () => {
+    if (event.target.className == "checkbox_example") {
+        if (event.target.value == "false") {
+            event.target.value = "true"
+        } else{
+            event.target.value = "false"
+        }
+        sum = 0
+        console.log(event.target.value)
+        console.log(inputs)
+
+        for (let i = 0; i < inputs.length; i++) {
+            let input = inputs[i]
+            console.log(input)
+            console.log(Number(input.dataset.price))
+            
+            if (input.value == "true") {
+                sum += Number(input.dataset.price) * Number(input.dataset.count)
+            }          
+                   
+        }
+        BlockSum.textContent = `
+            ${sum}
+        `
+
+    }
+})
+
